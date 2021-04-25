a10185a43852fa8c425984f05ed8cfbc958abce4
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2021-04-25 13:50:14 +0700

    Revert back to html

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 						<nav class="col-md-6 col-12 tm-nav">
 							<ul class="tm-nav-ul">
 								<li class="tm-nav-li"><a href="index.php" class="tm-nav-link active">Home</a></li>
-								<li class="tm-nav-li"><a href="about.php" class="tm-nav-link">About</a></li>
-								<li class="tm-nav-li"><a href="contact.php" class="tm-nav-link">Contact</a></li>
+								<li class="tm-nav-li"><a href="about.html" class="tm-nav-link">About</a></li>
+								<li class="tm-nav-li"><a href="contact.html" class="tm-nav-link">Contact</a></li>
 							</ul>
 						</nav>
 					</div>
